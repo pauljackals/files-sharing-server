c1dceeea04f88efa2190c0419b200809cf7c9bdb
pauljackals <paul.jackals@gmail.com>
2022-05-02 20:31:40 +0200

    shared directory

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,20 @@
 const router = require("express").Router()
+const express = require("express")
+const fs = require("fs")
 const { handleUnknownRoutes, authenticateUser, authorizeAdmin } = require("../utils/middlewares")
 const access = require("./access")
 const admin = require("./admin/admin")
+const sharedDirectory = "shared"
+
+if(!fs.existsSync(sharedDirectory)) {
+    fs.mkdirSync(sharedDirectory)
+}
 
 router
     .use("/access", access)
     .use("/admin", authenticateUser, authorizeAdmin, admin)
+    .use("/directory", authenticateUser)
+    .use("/shared", authenticateUser, express.static(sharedDirectory))
     .use(handleUnknownRoutes)
 
 module.exports = router
