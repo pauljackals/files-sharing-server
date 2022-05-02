132ba75174d12f06222c773714c8906a0ab0e9fb
pauljackals <paul.jackals@gmail.com>
2022-05-02 15:11:05 +0200

    unknown route handler

--- routes/router.js
+++ routes/router.js
@@ -3,5 +3,8 @@ const access = require("./access")
 
 router
     .use("/access", access)
+    .use((req, res, next) => {
+        next(new Error("route not found"))
+    })
 
 module.exports = router
