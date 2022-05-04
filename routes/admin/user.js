e02a3c6cb74ecdb37837b713511f940c6e8e529f
pauljackals <paul.jackals@gmail.com>
2022-05-04 12:48:35 +0200

    all users getter

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -6,14 +6,21 @@ const { NotFoundError, AuthorizationError } = require("../../utils/errors")
 const router = require("express").Router()
 
 router
+    .get("/", (req, res, next) => {
+        User.find().exec()
+            .then(users => {
+                res.status(200).json({users})
+            })
+            .catch(err => next(err))
+    })
     .delete("/:id", (req, res, next) => {
         const {id} = req.params
         User.findById(id).exec()
             .then(user => {
                 if(!user) {
                     throw new NotFoundError("user not found")
-                } else if(user.username === rootUsername) {
-                    throw new AuthorizationError("cannot remove root user")
+                } else if(user._id.toString()===req.user._id.toString() || user.username===rootUsername) {
+                    throw new AuthorizationError("cannot remove user")
                 }
                 return User.findByIdAndDelete(user._id).exec()
                     .then(() => Code.findOneAndDelete({user}).exec()
